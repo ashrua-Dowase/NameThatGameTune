19318c9a28614f53f3c02fdf4b78c16dfb0ba604
ashrua-Dowase <161979241+ashrua-Dowase@users.noreply.github.com>
2024-03-13 22:34:52 -0700

    Fixed issue with app not closing fully

--- src/main/java/nameThatTune/screen/dowaseSplash.java
+++ src/main/java/nameThatTune/screen/dowaseSplash.java
@@ -3,6 +3,8 @@ package nameThatTune.screen;
 import java.io.File;
 import java.io.FileInputStream;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -16,6 +18,7 @@ import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.util.Duration;
 import javafx.scene.image.*;
 import javafx.animation.FadeTransition;
@@ -35,6 +38,13 @@ public class dowaseSplash extends Application {
         stage.setFullScreen(true);
         stage.setTitle("Name that Tune! (VGE)");
         stage.show();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
         bgurl.close();
         MediaPlayer titleScreenMusic = playSong();
         dowaseLogo(scene,stage,splashScreen,titleScreenMusic);
